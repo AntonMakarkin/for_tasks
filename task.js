978a9ba9fddb486efe6c5e4b5af93e76d4e2ae72
AntonMakarkin <amakarkin27@gmail.com>
2019-09-05 00:14:57 +0300

    for_task

---
+++ task.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+})();
+
+var money = prompt("Ваш бюджет на месяц?","");
+var time = prompt("Введите дату в формате YYYY-MM-DD","");
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses:{},
+    optionalExpenses:{},
+    income:[],
+    savings:false
+};
+
+var expense_point_1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+var expense_cost_1 = prompt("Во сколько это обойдется?", "");
+var expense_point_2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+var expense_cost_2 = prompt("Во сколько это обойдется?", "");
+
+appData.expenses.expense_point_1 = expense_cost_1;
+appData.expenses.expense_point_2 = expense_cost_2;
+
+alert(expense_cost/30);
+
+
+
+
+
