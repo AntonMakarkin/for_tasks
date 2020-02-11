9cda0e46e87da3f2c031a426fb1b53fe02ca768c
AntonMakarkin <amakarkin27@gmail.com>
2020-02-11 23:55:02 +0300

    Работа js с элементами на страице (часть 1) и преобразования типов

---
+++ DOM.js
@@ -0,0 +1,5 @@
+let box = document.getElementById(''), //берем документ, получаем элемент по его id
+    button = document.getElementsByTagName(''); //береме документ, получаемые коллекуию элементов по тегу (псевдомассив)
+    circle = document.getElementsByClassName(''), //береме документ, получаемые коллекуию элементов по классу (псевдомассив)
+    heart = document.querySelectorAll('.'), //береме документ, получаемые коллекуию элементов по селектору (псевдомассив)
+    onHeart = document.querySelector(''); //берем документ, получаем элемент по его селектору
---
+++ Preobrazovaniya.js
@@ -0,0 +1,46 @@
+//to String
+
+//1)
+//String(null);
+//console.log(typeof(String(4)));
+
+//2)
+/*console.log("ww" + 5);
+console.log(typeof("q" + false));
+console.log(typeof("" + 5));*/
+
+//to Number
+
+//1)
+/*Number('5');
+console.log(typeof(Number('5')));*/
+//2)
+//console.log(typeof(5 + +'5')); //унарный плюс перед символом
+//3)
+//console.log(typeof(parseInt('15px', 10)));
+//let a = parseInt('15px');
+//console.log(a);
+
+//let ans = +prompt("How old are you?", ''); //преобразует введенную пользователем строку в число
+
+//0, '', null, undefined, NaN //false, остальное true
+
+/*let switcher = null;
+
+if(switcher) {
+    console.log("Working..")
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log("Working..")
+}*/
+
+//2)
+//console.log(typeof(Boolean('5')));
+
+//3)
+//console.log(typeof(!!'5'));
+
+//ИЛИ всегда возвращает первое правдивое утверждение, И всегда возвращает первое ложное утверждение
