de0df613b172dd71001a8b883fed8af8a87c1bb0
AntonMakarkin <amakarkin27@gmail.com>
2020-02-14 21:53:12 +0300

    Задание на отработку действий со страницей

--- DOM.js
+++ DOM.js
@@ -2,4 +2,35 @@ let box = document.getElementById(''), //берем документ, получ
     button = document.getElementsByTagName(''); //береме документ, получаемые коллекуию элементов по тегу (псевдомассив)
     circle = document.getElementsByClassName(''), //береме документ, получаемые коллекуию элементов по классу (псевдомассив)
     heart = document.querySelectorAll('.'), //береме документ, получаемые коллекуию элементов по селектору (псевдомассив)
-    onHeart = document.querySelector(''); //берем документ, получаем элемент по его селектору
+    onHeart = document.querySelector(''), //берем документ, получаем элемент по его селектору
+    wrapper = document.querySelector('wrapper.');
+box.style.backgroundColor = 'blue';
+button[1].style.borderRadius = '100%';
+
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+/*for (let i = 0; i < heart.length; i++) {
+    heart[i].style.backgroundColor = 'blue';
+}*/
+
+/*heart.forEach(function(item, i, hearts) {
+    item.style.backgroundColor = 'blue';
+});*/
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+//document.body.appendChild(div);
+//wrapper.appendChild(div);
+
+//div.innerHTML = '<h1>Hello</h1>';
+div.textContent = 'Hello World!';
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+
+document.body.replaceChild(button[1], circle[1]);
