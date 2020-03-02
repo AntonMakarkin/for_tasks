5bb58f51f037c6b6f89339f48a4cc8e48683b183
AntonMakarkin <amakarkin27@gmail.com>
2020-03-03 00:08:15 +0300

    Сделал рабочий таймер

--- yoga-site(задание по табам)/script.js
+++ yoga-site(задание по табам)/script.js
@@ -32,5 +32,61 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
         }
-    })
+    });
+
+    // Timer
+
+    let deadLine = '2020-03-10';
+
+    function getTimeRemaining(endtime) { //определяем остаток времени
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60), //Округляем до ближайшего меньшего целого, переводим мс в с и вычлиняем остаток(узнав кол-во минут)
+            minutes = Math.floor((t/1000/60) % 60), //вычленили минуты
+            hours = Math.floor((t/(1000 * 60 * 60))); //вычленили часы
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    //динамически меняем верстку, создавая таймер
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+        
+        //обновляем таймер
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) { //если число меньше 10, то подставляем перед ним 0
+                if (num <= 9) {
+                    return '0' + num;
+                }
+                else {
+                    return num;
+                }
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
 });
