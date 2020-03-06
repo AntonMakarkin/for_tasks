ceccc2a7870468c647d1c60398de7c2dfe54d4eb
AntonMakarkin <amakarkin27@gmail.com>
2020-03-06 15:28:01 +0300

    Прототип игры Морской бой

--- Constructor.js
+++ Constructor.js
@@ -19,11 +19,25 @@ let ivan = new User('Ivan', 25),
 console.log(ivan);
 console.log(alex);
 
-ivan.exit();
-*/
+ivan.exit();*/
+
+//Контексты вызова
+
+function showThis() {
+    console.log(this);
+    function sum() {
+        console.log(this);
+        //return this.a + this.b;
+        return a + b; //пример с замыканием - функция не видит переменные и ищет их на слой выше
+    }
+    console.log(sum());
+}
+showThis(4,5);
+showThis(5,5);
+
 
 //Код на ЕS6
-class Users {
+/*class Users {
     constructor(name, id) {
         this.name = name;
         this.id = id;
@@ -41,4 +55,4 @@ let alex = new User('Alex', 20);
 console.log(ivan);
 console.log(alex);
 ivan.hello();
-alex.hello();
+alex.hello();*/
