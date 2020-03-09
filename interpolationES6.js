e0e064b52a7590a3b6c6d8cbbbadef8fa113aeed
AntonMakarkin <amakarkin27@gmail.com>
2020-03-09 14:50:40 +0300

    Интерполяция ES6

---
+++ interpolationES6.js
@@ -0,0 +1,8 @@
+let name = 'Ivan',
+    age = 30,
+    mail = 'ex@mail.ru';
+
+document.write('Пользователю' + name + ' ' + age + ' лет. Его почтовый адрес: ' + mail);    
+
+//ES6
+document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
