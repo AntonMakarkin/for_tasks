f67f5856e155ad680b2b8ebd28773730268b1d6c
AntonMakarkin <amakarkin27@gmail.com>
2020-04-04 12:46:08 +0300

    Доработал калькулятор и начал изучать localstorage

--- yoga-site(задание по табам)/script.js
+++ yoga-site(задание по табам)/script.js
@@ -328,16 +328,57 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    //Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0,
+        city = 0;
+
+        totalValue.innerHTML = 0;
+        
+        persons.addEventListener('input', function() {
+            personsSum = +this.value;
+
+            if(restDays.value == '' || personsSum == 0) {
+                totalValue.innerHTML = 0;
+            }
+            else {
+                city = place.value;
+                total = ((daysSum + personsSum) * 4000) * city;
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('input', function () {
+           daysSum = +this.value;
+           total = (daysSum + personsSum) * 4000;
+
+            if (persons.value == '' || daysSum == 0) {
+                totalValue.innerHTML = 0;
+            }
+            else {
+                city = place.value;
+                total = ((daysSum + personsSum) * 4000) * city;
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function() {
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            }
+            else {
+                personsSum = +persons.value;
+                daysSum = +restDays.value;
+                city = this.options[this.selectedIndex].value;
+                total = ((daysSum + personsSum) * 4000) * city;
+                totalValue.innerHTML = total;
+            }
+        });
 });
 
-// Второе задание
-
- /*<input id="age" value="30">
- 
- let age = document.getElementById('age');
-
- function showUser(surname, name) {
-          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-}
-
-showUser.apply(age, ["Горький","Максим"]);*/
